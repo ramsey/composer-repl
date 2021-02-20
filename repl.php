8585ac18d47fa8cc6662d202167bd8c0c852b337
Ben Ramsey <ben@benramsey.com>
2021-02-19 20:35:20 -0600

    chore: add comment to repl.php to explain its purpose

--- repl.php
+++ repl.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * This file exists as an example for environment bootstrapping, and it also
+ * doubles as an aid in testing environment bootstrapping.
+ *
+ * @link https://github.com/ramsey/composer-repl#environment-bootstrapping
+ * @see \Ramsey\Test\Dev\Repl\Composer\ReplCommandTest::testRun()
+ */
+
 declare(strict_types=1);
 
 $foo = 'bar';
